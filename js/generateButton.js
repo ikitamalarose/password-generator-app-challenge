6bea97f8618d833a1e870f6ef24628c2197615bb
ikitamalarose <laroseikitama@gmail.com>
2024-07-19 12:55:13 +0000

    Implement validation for form elements and handle error display
    - Added functions to validate slider length, selected state, and checkbox choices.
    - Updated  to check if at least one of each option is selected.
    - Added error handling to display a message when validation fails.
    - Modified CSS display handling for the error message.
    - Updated JavaScript to show and hide the error message appropriately.

--- js/generateButton.js
+++ js/generateButton.js
@@ -4,6 +4,8 @@ import { getSliderLength } from "./slider.js";
 
 const generateButton = document.getElementById('generate-button');
 
+const errorMessage = document.querySelector('.error-message');
+
 export function initGenerateButton() {
 
     generateButton.addEventListener('click', () => {
@@ -11,13 +13,41 @@ export function initGenerateButton() {
         const selectedState = getSelectedState();
         const currentSliderLength = getSliderLength();
 
-        console.log("slider length : ", currentSliderLength);
-        console.log("selected state : ", selectedState);
-        console.log("choix checkbox  : ");
+        if (isSliderLengthValid(currentSliderLength) && isStateSelectedValid(selectedState) && isCheckboxChoicesValid(checkboxChoices)) {
+            hideErrorMessage();
+            
+            console.log("slider length : ", currentSliderLength);
+            console.log("selected state : ", selectedState);
+            console.log("choix checkbox  : ");
 
-        checkboxChoices.forEach(checkbox => {
-            console.log();
-            console.log(checkbox.id);
-        });
+            checkboxChoices.forEach(checkbox => {
+                console.log(checkbox.id);
+            });
+        } else {
+            showErrorMessage();
+        }
+
     });
+}
+
+
+function isSliderLengthValid(length) {
+    return length !== 0;
+}
+
+function isStateSelectedValid(state) {
+    return state !== null;
+}
+
+function isCheckboxChoicesValid(choices) {
+    return choices !== null;
+}
+
+function showErrorMessage() {
+    errorMessage.textContent = 'You must select at least one of each option.';
+    errorMessage.style.display = 'block';
+}
+
+function hideErrorMessage(){
+    errorMessage.style.display = 'none';
 }
